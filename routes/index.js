7e0a0609fd99e122af2261bd5d7890ece82b6dc9
Ray Yang <>
2020-09-27 21:03:00 +1000

    Added some comments on index router

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ const messages=require('../errorMessage');
 
 router.post('/', function(req, res, next) {
 
+  // Check if payload node exists
   let shows=req.body.payload;
   if (shows===undefined){
      res.status(400)
      return res.json(messages.payloadIsMissing);
   }
   
-
+  // Check if payload is array or not, if it is an array, check if the array is empty or not
   if (shows.length ===undefined || shows.length<1){
     res.status(400)
     return res.json(messages.payloadIsNull)
